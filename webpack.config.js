3fcb9b10a5a7c694894a3bea0b4275226833dad0
Aldur <aldur@zuminternet.com>
2018-12-13 20:22:41 +0900

    scss output style 변경, 주석정리

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,8 @@ module.exports = (env, options) => {
             { 
               loader: 'sass-loader',
               options: {
-                sourceMap: true
+                sourceMap: true,
+                outputStyle: 'expanded'
               }
             }
           ]
